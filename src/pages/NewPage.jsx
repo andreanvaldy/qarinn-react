36e69f7f03599733abaf9d8020ff4c17ec5c3584
andreanvaldy <andrefiliate@gmail.com>
2024-12-10 09:41:35 +0700

    jj

---
+++ src/pages/NewPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewPage.css';
+
+
+function NewPage() {
+  return (
+    <div>
+      <h1>Halo dari NewPage!</h1>
+      <p>Ini adalah halaman baru yang dibuat secara manual di folder `src`.</p>
+    </div>
+  );
+}
+
+export default NewPage;
